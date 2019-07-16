b0f61179087df8a382625276febad4f91f36ec49
aandresrafael <aandresrafae@gmail.com>
2019-07-16 18:46:09 -0500

    Set some relationships

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,2 +1,6 @@
 class Unit < ApplicationRecord
+  belongs_to :unit_group
+  belongs_to :channel_rate
+  has_many :unit_amenities
+
 end
--- app/models/unit_group.rb
+++ app/models/unit_group.rb
@@ -1,2 +1,8 @@
 class UnitGroup < ApplicationRecord
+  belongs_to :channel_rate
+  belongs_to :unit_type
+  has_many :units
+  has_many :invoiceable_fees
+  has_many :unit_amenities
+  has_many :discount_plans
 end
